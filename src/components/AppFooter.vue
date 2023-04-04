c33cc6ff847db44ef74851b158c61b102f30cef8
Carlos Osoria <carlos.osoria@gmail.com>
2023-04-03 21:39:35 -0500

    Added the header and footer components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,35 @@
+<template>
+<footer class="py-5 bg-dark">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3 bgx-primary">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-white" >Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-white">About</a></li>
+    </ul>
+    <div class="container"><p class="m-0 text-center text-white">Copyright © Your Website 2022</p></div>
+</footer>
+
+
+
+<!-- 
+    <footer class="py-3 my-4">
+    <ul class="nav navbar-nav justify-content-center border-bottom pb-3 mb-3 bgx-primary">
+      <li class="nav-item"><a href="#" class="nav-link px-2">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2">About</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">© 2023 Company, Inc</p>
+  </footer> -->
+</template>
+
+<script>
+    export default {
+        name: "AppFooter",
+    }
+</script>
+
+<style scoped>
+</style>
